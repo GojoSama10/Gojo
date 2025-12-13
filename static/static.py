6c1805859eca0822719dd314983e9209d14a40bc
GojoSama10 <alessandro2amato@gmail.com>
2025-12-13 16:18:23 +0100

    Update static.py

--- static/static.py
+++ static/static.py
@@ -1,5 +1,5 @@
-# static.py
-
 HTML = """
-The girl was wearing a denim jacket over a denim shirt and denim jeans.
+The girl was wearing a denim jacket over a denim shirt and denim jeans
+I was about to give up but then i went to school again
+I'll walk in front of you because i love you
 """
